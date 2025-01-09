135c387a1d05a502979b9e4f5dd264a536e60816
macbook <macbook@macbooks-MBP.lan>
2025-01-09 16:50:50 -0500

    Updated files and settins

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-       home: const LoginPage(), // Default home page
+       home: const HomePage(), // Default home page
        scaffoldMessengerKey: scaffoldMessengerKey,
       navigatorObservers: [routeObserver], // Route observer
       routes: {
